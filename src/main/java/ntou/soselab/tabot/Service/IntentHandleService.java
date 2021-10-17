17c809b9d40088f868f5bef76e654488e380a957
david02653 <david02653@gmail.com>
2021-10-17 22:43:15 +0800

    add intent handle (incomplete)

--- src/main/java/ntou/soselab/tabot/Service/IntentHandleService.java
+++ src/main/java/ntou/soselab/tabot/Service/IntentHandleService.java
@@ -1,4 +1,22 @@
 package ntou.soselab.tabot.Service;
 
+import ntou.soselab.tabot.Entity.Rasa.Intent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
 public class IntentHandleService {
+
+    @Autowired
+    public IntentHandleService(Environment env){
+    }
+
+    /**
+     * declare what bot should do with each intent
+     * @param intent incoming intent
+     */
+    public void checkIntent(Intent intent){
+        // todo: check intent and do stuff
+    }
 }
